4e11a759883815fc9bfc57b1a0d31dd43bdb7320
Rcboy-hub <harishentreprenur@gmail.com>
2025-04-17 16:19:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,12 +28,12 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/f4f85b3aaf6b6b1e4817a.jpg')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://iili.io/2rCQZpn.jpg https://iili.io/2UzdkkQ.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/60e8a622b14796e4448ce.mp4")
+MELCOW_VID = environ.get("MELCOW_VID", "https://iili.io/24Uo4yP.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/242b7f1b52743938d81f1.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://iili.io/2iBGyfs.jpg https://iili.io/2iBGjlj.jpg')).split()  # Fsub pic
 
 # ============================
 # Admin, Channels & Users Configuration
