faa92a6e86c790f42564339c0972820666734969
Rcboy-hub <harishentreprenur@gmail.com>
2025-04-18 17:23:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', '')
+QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/hFmm5F4B/file-1174.jpg')
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'harishbharath89@oksbi')
 
 # ============================
