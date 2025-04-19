a2b05afa104d9fd40cf6eced6cdf810b80eb86cf
Rcboy-hub <harishentreprenur@gmail.com>
2025-04-19 16:45:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -76,7 +76,7 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        m=await message.reply_sticker("https://iili.io/24Uo4yP.jpg")
+        m=await message.reply_sticker("")
         await asyncio.sleep(0.4)
         await m.delete()        
         await message.reply_photo(
@@ -113,7 +113,7 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        m=await message.reply_sticker("https://iili.io/24Uo4yP.jpg")
+        m=await message.reply_sticker("")
         await asyncio.sleep(0.4)
         await m.delete()        
         await message.reply_photo(
