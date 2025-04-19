772956007020ae019fe75e674e967a2f18203d7f
Rcboy-hub <harishentreprenur@gmail.com>
2025-04-19 16:39:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -76,7 +76,7 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        m=await message.reply_sticker("CAACAgUAAxkBAAIVX2X0hg3ALXpkts2CL2TuuPZKsGYbAAIGCQACKnkRVoDtd_tvs2qRNAQ")
+        m=await message.reply_sticker("")
         await asyncio.sleep(0.4)
         await m.delete()        
         await message.reply_photo(
@@ -113,7 +113,7 @@ async def start(client, message):
             gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
         else:
             gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        m=await message.reply_sticker("CAACAgUAAxkBAAIVX2X0hg3ALXpkts2CL2TuuPZKsGYbAAIGCQACKnkRVoDtd_tvs2qRNAQ")
+        m=await message.reply_sticker("https://iili.io/24Uo4yP.jpg")
         await asyncio.sleep(0.4)
         await m.delete()        
         await message.reply_photo(
